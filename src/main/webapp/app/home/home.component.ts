108f185e08d24bf0234836eaeedddb537686dacc
Gyles Polloso <gyles.polloso@gmail.com>
2019-05-30 00:49:33 +1200

    Add swiper to home page

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -13,6 +13,19 @@ export class HomeComponent implements OnInit {
   account: Account;
   modalRef: NgbModalRef;
 
+  title = 'NG7Swiper';
+
+  config: SwiperOptions = {
+    autoplay: 3000, // Autoplay option having value in milliseconds
+    initialSlide: 1, // Slide Index Starting from 0
+    slidesPerView: 1, // Slides Visible in Single View Default is 1
+    pagination: '.swiper-pagination', // Pagination Class defined
+    paginationClickable: true, // Making pagination dots clicable
+    nextButton: '.swiper-button-next', // Class for next button
+    prevButton: '.swiper-button-prev', // Class for prev button
+    spaceBetween: 30 // Space between each Item
+  };
+
   constructor(
     private accountService: AccountService,
     private loginModalService: LoginModalService,
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SwiperModule } from 'angular2-useful-swiper';
 
 import { MyshowcaseSharedModule } from 'app/shared';
 import { HOME_ROUTE, HomeComponent } from './';
 
 @NgModule({
-  imports: [MyshowcaseSharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [MyshowcaseSharedModule, SwiperModule, RouterModule.forChild([HOME_ROUTE])],
   declarations: [HomeComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
