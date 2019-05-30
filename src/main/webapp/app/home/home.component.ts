226d1985e6b252b570801e3b9c0b3a0f0bc261fe
Gyles Polloso <gyles.polloso@gmail.com>
2019-05-31 00:03:35 +1200

    Add content

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -16,14 +16,14 @@ export class HomeComponent implements OnInit {
   title = 'NG7Swiper';
 
   config: SwiperOptions = {
-    autoplay: 3000, // Autoplay option having value in milliseconds
+    autoplay: 1200000, // Autoplay option having value in milliseconds
     initialSlide: 1, // Slide Index Starting from 0
     slidesPerView: 1, // Slides Visible in Single View Default is 1
     pagination: '.swiper-pagination', // Pagination Class defined
     paginationClickable: true, // Making pagination dots clicable
     nextButton: '.swiper-button-next', // Class for next button
     prevButton: '.swiper-button-prev', // Class for prev button
-    spaceBetween: 30 // Space between each Item
+    spaceBetween: 10 // Space between each Item
   };
 
   constructor(
